7952ef690c6c83d3a1e6c249e90de44b23530c29
Dong Gil, Seo <sirospace@sirospace.com>
2013-03-10 01:05:36 +0900

    Re Commit 0.1

---
+++ Pure Developer Preview 0.2/install-recovery.sh
@@ -0,0 +1,33 @@
+#!/system/bin/sh
+######################################################################
+###################### DreamNarae Pure 1.0 ###########################
+#################################Best of DreamNarae Tweak#############
+## Made by Sopiane(Dong Gil, Seo)
+## and Rie (Eun Hee, Lee)
+## and Iren (Jung Yeon, Lee)
+## and Ayana (Eun Jung, Lee)
+## With four people going to make the best Android Tweak!
+## Supported by Colorful Harmony Team- Angeloid Team
+## Thanks to Developer, and User.
+## This Version is Final Version We made.
+## Enjoy Your Device With DreamNarae!
+######################################################################
+mount -o rw,remount /system
+run-parts /system/etc/init.d
+echo "exit with code 111 is Complete, Please Check!"
+echo "######################################################################"
+echo "###################### DreamNarae Pure 1.0 ###########################"
+echo "#################################Best of DreamNarae Tweak#############"
+echo "## Made by Sopiane(Dong Gil, Seo)"
+echo "## and Rie (Eun Hee, Lee)"
+echo "## and Iren (Jung Yeon, Lee)"
+echo "## and Ayana (Eun Jung, Lee)"
+echo "## With four people going to make the best Android Tweak!"
+echo "## Supported by Colorful Harmony Team- Angeloid Team"
+echo "## Thanks to Developer, and User."
+echo "## This Version is Final Version We made."
+echo "## Enjoy Your Device With DreamNarae!"
+echo "######################################################################"
+echo "Thank you Choose DreamNarae Pure!"
+echo "OK, Just Reboot your Device, and Enjoy Your Device!"
+echo "and, bye! :D"
